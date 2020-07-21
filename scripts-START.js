47c505160f361fcfc69456621271cc2aba70962a
copiousamounts <nick.hernstedt@gmail.com>
2020-07-21 13:38:17 -0700

    now shows remainder seconds

--- scripts-START.js
+++ scripts-START.js
@@ -3,6 +3,7 @@ let countdown;
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -12,5 +13,12 @@ function timer(seconds) {
       clearInterval(countdown);
       return;
     }
-  });
+    //display it
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSEconds = seconds % 60;
 }
