8cd149e5a52e89efc3a78716f109e57426f44011
copiousamounts <nick.hernstedt@gmail.com>
2020-07-21 13:51:27 -0700

    countdown timer and be back at now working

--- scripts-START.js
+++ scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
- onxg timerDisplay = document.querySelector('.dispaly__time-left')
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -21,5 +23,18 @@ function timer(seconds) {
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
-  const remainderSEconds = seconds % 60;
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}: ${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+  timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
 }
