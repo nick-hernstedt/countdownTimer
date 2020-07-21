21358ae1029961df1286bb729347a99fcc3b321a
copiousamounts <nick.hernstedt@gmail.com>
2020-07-21 14:00:01 -0700

    all buttons now work, can also add custom timer

--- scripts-START.js
+++ scripts-START.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
 const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time");
 
 function timer(seconds) {
+  //clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
@@ -38,3 +42,16 @@ function displayEndTime(timestamp) {
     minutes < 10 ? "0" : ""
   }${minutes}`;
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+document.customForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
