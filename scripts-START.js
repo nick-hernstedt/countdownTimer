650c32592c2bee96e2c6e752f1751b07d68fca39
copiousamounts <nick.hernstedt@gmail.com>
2020-07-21 13:36:39 -0700

    now have countdown function working

---
+++ scripts-START.js
@@ -0,0 +1,16 @@
+let countdown;
+
+function timer(seconds) {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    //Check if we should stop it
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+  });
+}
